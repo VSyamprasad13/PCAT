13c299ffb3d4341b50d447b960ba220d128ba628
VSyamprasad13 <166799132+VSyamprasad13@users.noreply.github.com>
2024-04-16 09:26:46 +0530

    Update Grading.java

--- Daily Assignments/Grading.java
+++ Daily Assignments/Grading.java
@@ -1,34 +1,20 @@
 import java.util.Scanner;
-public class Grading
-{
-	public static void main(String[] args) 
-	{
+public class Grading{
+	public static void main(String[] args) {
   		Scanner sc=new Scanner(System.in);
-      System.out.println("Enter the student marks");
-	    int marks=sc.nextInt();
-	    if(marks>=90 && marks<=100)
-  		{
+		System.out.println("Enter the student marks");
+		int marks=sc.nextInt();
+	    	if(marks>=90 && marks<=100)
   		   System.out.println("A grade");
-  		}
   		else if(marks>=80 && marks<90)
-  		{
   		   System.out.println("B grade");
-  		}
   		else if(marks>=70 && marks<80)
-  		{
   		   System.out.println("C grade");
-  		}
   		else if(marks>=60 && marks<70)
-  		{
   		   System.out.println("D grade");
-  		}
   		else if(marks<60)
-  		{
   			System.out.println("failed");
-  		}
   		else
-  		{
   			System.out.println("Error message");
-  		}
   	}
 }
