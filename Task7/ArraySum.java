9a85fecc2a67a43ef93d80ee5f235509f1c2af2a
VSyamprasad13 <166799132+VSyamprasad13@users.noreply.github.com>
2024-04-18 13:10:49 +0530

    Add files via upload

---
+++ Task7/ArraySum.java
@@ -0,0 +1,15 @@
+package com.array;
+//Task 6.a
+public class ArraySum {
+
+	public static void main(String[] args) {
+		int[] arr= {2,8,1,9,50,15,3};
+		System.out.println(sumArray(arr, 0, arr.length-1));
+	}
+	public static int sumArray(int[] arr,int i, int j) {
+		if(i>j)
+		return 0;
+		else return sumArray(arr, i+1,j)+arr[i];
+	}
+
+}
