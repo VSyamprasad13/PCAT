1606d5c2ba04a8f28e05a283c07b08d71b11e8d6
VSyamprasad13 <166799132+VSyamprasad13@users.noreply.github.com>
2024-04-19 09:18:22 +0530

    Update ArrayRev.java

--- Task 4/ArrayRev.java
+++ Task 4/ArrayRev.java
@@ -4,7 +4,7 @@ package com.array;
 public class ArrayRev {
 
 	public static void main(String[] args) {
-		int[] arr= {2,7,8,4,10,3,1};
+		int[] arr= {1,2,3,4,5,6,7};
 		int[] temp=new int[arr.length];
 		
 		for(int i=arr.length-1,j=0;i>=0;i--) {
